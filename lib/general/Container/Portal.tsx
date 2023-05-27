7865ec72fe6868953cbffdedc3f7a7628c278664
victorlamprojects <victorlamprojects@gmail.com>
2023-05-27 19:23:57 +0800

    added portal

---
+++ lib/general/Container/Portal.tsx
@@ -0,0 +1,14 @@
+import { FC, ReactNode } from "react";
+import { createPortal } from "react-dom";
+
+interface PortalProps {
+    children: ReactNode
+};
+
+const Portal:FC<PortalProps> = ({children}) => {
+    const documentDiv = document.createElement("div");
+	document.getElementById("root").append(documentDiv);
+    return createPortal(children, documentDiv);
+}
+
+export default Portal;
