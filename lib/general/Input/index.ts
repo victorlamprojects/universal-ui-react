43dd5183c3c967234a7593ecc23026b391b9467e
victorlamprojects <victorlamprojects@gmail.com>
2024-04-18 02:18:05 +0800

    exported Select type

--- lib/general/Input/index.ts
+++ lib/general/Input/index.ts
@@ -3,3 +3,4 @@ export { default as TextInput } from "./TextInput";
 export { default as DateInput } from "./DateInput";
 export { default as SwitchInput } from "./SwitchInput";
 export { default as Select } from "./Select";
+export * from "./Select";
