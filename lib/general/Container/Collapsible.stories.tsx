d3d831bf62af5f7caf554846a30f3f98fc5875fb
victorlamprojects <victorlamprojects@gmail.com>
2024-04-20 02:28:43 +0800

    added collapsible container

---
+++ lib/general/Container/Collapsible.stories.tsx
@@ -0,0 +1,43 @@
+import React, { useState, ComponentProps } from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+
+import Collapsible from './Collapsible';
+import Block from '../Container/Block';
+import { getTheme } from '../../theme/theme';
+
+const meta: Meta<typeof Collapsible> = {
+	title: 'VictorLam/Collapsible',
+	component: Collapsible
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Collapsible>;
+
+export const CollapsibleStory: Story = {
+	args: {
+		title: "Collapsible Text"
+	},
+	render: (args: ComponentProps<typeof Collapsible>) => {
+		return (<div>
+			<ThemeProvider theme={getTheme("light")}>
+				<Block>
+					<Collapsible {...args}>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non purus lorem. Mauris gravida eu ante nec semper. Morbi ullamcorper magna vitae quam iaculis pretium. Vivamus feugiat tincidunt sem, et lacinia metus. Nulla a pulvinar augue. Sed eu turpis nec est tempus imperdiet vel vitae tortor. Quisque fringilla, tortor at semper gravida, erat dui pulvinar nulla, viverra posuere metus felis sit amet augue. Aenean id fermentum lectus. Curabitur feugiat posuere leo eget venenatis. Vivamus eu ligula nec elit porta maximus. Vivamus rhoncus sollicitudin ullamcorper. Curabitur ut feugiat arcu, eu dictum justo. Praesent luctus odio et libero dictum volutpat. Curabitur suscipit varius facilisis. Etiam ex diam, sollicitudin sit amet fringilla et, pellentesque quis orci. Nunc pulvinar tortor a facilisis egestas.
+					</Collapsible>
+				</Block>
+			</ThemeProvider>
+			<br/>
+			<br/>
+			<br/>
+			<ThemeProvider theme={getTheme("dark")}>
+				<Block>
+					<Collapsible {...args}>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non purus lorem. Mauris gravida eu ante nec semper. Morbi ullamcorper magna vitae quam iaculis pretium. Vivamus feugiat tincidunt sem, et lacinia metus. Nulla a pulvinar augue. Sed eu turpis nec est tempus imperdiet vel vitae tortor. Quisque fringilla, tortor at semper gravida, erat dui pulvinar nulla, viverra posuere metus felis sit amet augue. Aenean id fermentum lectus. Curabitur feugiat posuere leo eget venenatis. Vivamus eu ligula nec elit porta maximus. Vivamus rhoncus sollicitudin ullamcorper. Curabitur ut feugiat arcu, eu dictum justo. Praesent luctus odio et libero dictum volutpat. Curabitur suscipit varius facilisis. Etiam ex diam, sollicitudin sit amet fringilla et, pellentesque quis orci. Nunc pulvinar tortor a facilisis egestas.
+					</Collapsible>
+				</Block>
+			</ThemeProvider>
+		</div>)
+	}
+}
---
+++ lib/general/Container/Collapsible.tsx
@@ -0,0 +1,71 @@
+import { FC, useState, HTMLAttributes } from "react";
+import { getDefaultThemeIfNotFound } from '../../theme/theme';
+import styled from "styled-components";
+
+const CollapsibleContainer = styled.div(({theme}) => {
+	theme = getDefaultThemeIfNotFound(theme);
+	return {
+		maxWidth: "100%"
+	};
+});
+
+const Title = styled.div(({theme}) => {
+	theme = getDefaultThemeIfNotFound(theme);
+	return {
+		width: "100%",
+		fontSize: "1.25rem",
+		filter: "alpha(opacity=75)",
+		cursor: "pointer",
+		padding: "0.5rem 0",
+		fontWeight: "bold",
+		userSelect: "none",
+		"&:hover": {
+			filter: "alpha(opacity=100)",
+		},
+		"&:before": {
+			content: "'▸  '"
+		},
+		"&.show": {
+			"&:before": {
+				content: "'▾  '"
+			}
+		}
+	};
+});
+
+const Content = styled.div(({theme}) => {
+	theme = getDefaultThemeIfNotFound(theme);
+	return {
+		width: "100%",
+		height: 0,
+		fontSize: "inherit",
+		overflowY: "hidden",
+		transition: "height 0.2s ease",
+		"&.show": {
+			height: "auto",
+			overflow: "visible"
+		}
+	};
+});
+
+interface CollapsibleProps extends HTMLAttributes<HTMLDivElement> {
+	title?: string;
+	collapsed?: boolean;
+	toggle?: boolean;
+};
+
+const Collapsible:FC<CollapsibleProps> = ({
+	title,
+	children,
+	collapsed=true,
+	toggle=false,
+	...args
+}) => {
+	const [show, setShow] = useState<boolean>(!collapsed);
+	return <CollapsibleContainer {...args}>
+		<Title className={show ? "show" : ""} onClick={() => setShow(prev=>!prev)}>{title}</Title>
+		<Content className={show ? "show" : ""}>{children}</Content>
+	</CollapsibleContainer>
+}
+
+export default Collapsible;
